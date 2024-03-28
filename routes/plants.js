1fa5b48c0be56cb826e54c074026ab907ea43733
Pratheek Vasu <pvasu1@sheffield.ac.uk>
2024-03-28 11:24:35 +0000

    Completed plant details page

--- routes/plants.js
+++ routes/plants.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Plant Sightings' });
-});
 
 // GET plant details page
 router.get('/plant', function(req, res, next) {
   res.render('plant_details', {title: 'Plant Details'});
 });
-
-
 
 module.exports = router;
