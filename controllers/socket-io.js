23e6e4fc877a89dd8d6e418cd42e082d59561c58
Pratheek Vasu <pvasu1@sheffield.ac.uk>
2024-03-29 17:01:03 +0000

    Added socket.io and functionality for join button

--- controllers/socket-io.js
+++ controllers/socket-io.js
@@ -1,23 +1,17 @@
 exports.init = function(io) {
-    io.sockets.on('connection', function (socket) {
-      console.log("try");
-      try {
-        /**
-         * create or joins a room
-         */
-        socket.on('create or join', function (room, userId) {
-          socket.join(room);
-          io.sockets.to(room).emit('joined', room, userId);
+
+    io.on('connection', function (socket) {
+        socket.on("newuser", function(username) {
+            socket.broadcast.emit("update", username + " joined the conversation");
         });
-  
-        socket.on('chat', function (room, userId, chatText) {
-          io.sockets.to(room).emit('chat', room, userId, chatText);
+
+        socket.on("exituser", function(username) {
+            socket.broadcast.emit("update", username + " left the conversation");
         });
-  
-        socket.on('disconnect', function(){
-          console.log('someone disconnected');
+
+        socket.on("chat", function(message) {
+            socket.broadcast.emit("chat", message);
         });
-      } catch (e) {
-      }
     });
-  }
+
+}
