ba2dfdeee7d93544d8d97e61484c594cdc793a76
Cameron <camjr246@yahoo.co.uk>
2024-04-09 12:19:01 +0100

    Added js file for form upload that converts image to base64 representation under field base64Image

---
+++ public/javascripts/formSubmission.js
@@ -0,0 +1,25 @@
+document.getElementById("add_plant").addEventListener("submit", function(e) {
+	e.preventDefault();
+	let imageUpload = document.getElementById("upload_photo");
+	image = imageUpload.files[0];
+
+	if (image) {
+		let reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = () => {
+			let base64 = reader.result;
+			console.log(base64)
+			let base64Input = document.createElement("input");
+			base64Input.setAttribute("type", "hidden");
+			base64Input.setAttribute("name", "base64Image");
+			base64Input.setAttribute("id", "base64Image");
+			base64Input.setAttribute("value", base64);
+			document.getElementById("add_plant").appendChild(base64Input);
+
+			document.getElementById("add_plant").submit();
+		};
+		reader.onerror = () => {
+			console.log("Error converting image");
+		};
+	};
+});
