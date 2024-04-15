0564bb7116ff8839e1fac7dd8fe51490de2ad7b8
Cameron <camjr246@yahoo.co.uk>
2024-04-15 20:28:28 +0100

    Created functionality to add to a username to indexedDB, if an entry exists it will be printed to console

--- public/javascripts/username.js
+++ public/javascripts/username.js
@@ -1,10 +1,43 @@
-function handleUpgrade() {
+const handleUpgrade = (ev) => {
+    const db = ev.target.result;
+    db.createObjectStore("username", { keyPath: "id", autoIncrement: true });
+    console.log("Upgraded object store");
+};
+
+const handleSuccess = () => {
+    const usernameIDB = requestIDB.result;
+    const transaction = usernameIDB.transaction(["username"], "readwrite");
+    const usernameStore = transaction.objectStore("username")
+    const getRequest = usernameStore.getAll()
+    getRequest.addEventListener("success", () => {
+        res = getRequest.result[0];
+        if (res === undefined) {
+            console.log(res);
+        } else {
+            console.log(`Found: ${res.username}`);
+        }
+    })
+
+    const usernameForm = document.getElementById("usernameForm");
+
+    usernameForm.addEventListener("submit", handleUsernameEntered)
 }
 
-function handleSuccess() {
-}
+const handleUsernameEntered = () => {
+    const username = document.getElementById("username").value;
+    if (username != "") {
+        const usernameIDB = requestIDB.result;
+        const transaction = usernameIDB.transaction(["username"], "readwrite");
+        const usernameStore = transaction.objectStore("username")
+        const addRequest = usernameStore.add({ username: username })
 
-const requestIDB = indexedDB.open("todo");
+        addRequest.addEventListener("success", () => {
+            console.log("Added username")
+        });
+    }
+};
+
+const requestIDB = indexedDB.open("plant");
 requestIDB.addEventListener("upgradeneeded", handleUpgrade);
 requestIDB.addEventListener("success", handleSuccess);
 requestIDB.addEventListener("error", (err) => {
