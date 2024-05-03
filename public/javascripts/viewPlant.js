8cf101a4d5508e4b731799671233131913d2fcf9
Cameron <camjr246@yahoo.co.uk>
2024-05-03 14:02:53 +0100

    Updated plant view page so update identification button is not shown to any user who isn't plant author

--- public/javascripts/viewPlant.js
+++ public/javascripts/viewPlant.js
@@ -1,19 +1,22 @@
 import { openUsernameIDB, getUsername } from "./idb-utility.js";
 
-window.onload = function(){
+window.onload = function() {
     openUsernameIDB().then((db) => {
         getUsername(db).then((res) => {
-            const sessionAuthor = res.value
-            const plantAuthor = document.getElementById("plant-user").innerText;
-            console.log(plantAuthor)
+            const sessionAuthor = res.value;
+            const plantAuthor = document.getElementById("plant-user").innerText.trim();
+            console.log(`Plant author: ${plantAuthor}`)
+            console.log(`Session author: ${sessionAuthor}`)
             if (plantAuthor === sessionAuthor) {
+                console.log("same")
                 // Append the update button to the page
                 const heading = document.getElementById("update_button_heading");
 
+                console.log("same")
                 // or retrieve the button element by giving it an id
                 // and use remove/set attribute to enable/disable it
                 const update_button = document.getElementById("update_button");
-                update_button.removeAttribute("disabled");
+                update_button.removeAttribute("hidden");
             } else {
                 // Don't append the update button to the page
 
