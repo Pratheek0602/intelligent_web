281711737db4d3954858845b12c6549550d5f967
Cameron <camjr246@yahoo.co.uk>
2024-05-03 14:01:57 +0100

    Updated service worker to include new js files

--- public/sw.js
+++ public/sw.js
@@ -56,6 +56,9 @@ self.addEventListener('install', event => {
         '/javascripts/chat.js',
         '/javascripts/index.js',
         '/javascripts/username.js',
+        '/javascripts/idb-utility.js',
+        '/javascripts/viewPlant.js',
+        '/javascripts/formSubmission.js',
       ]);
       console.log('Service Worker: App Shell Cached');
     } catch (error) {
