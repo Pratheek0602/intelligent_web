2a5c42a353fb28f4c38137b6b1483e58c83dbdf8
tanilandge <tlandge1@sheffield.ac.uk>
2024-05-14 20:56:08 +0100

    fixed username in add plannt page

--- public/javascripts/formSubmission.js
+++ public/javascripts/formSubmission.js
@@ -1,4 +1,63 @@
-import { addPlantToSync, getAllAddedPlantsToSync, deleteSyncedPlants, syncPlants } from './idb-utility.js';
+// import { addPlantToSync, openUsernameIDB, getUsername, getAllAddedPlantsToSync, deleteSyncedPlants, syncPlants } from './idb-utility.js';
+
+// document.getElementById("add_plant").addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     let imageUpload = document.getElementById("upload_photo");
+//     let image = imageUpload.files[0];
+
+//     if (image) {
+//         let reader = new FileReader();
+//         reader.readAsDataURL(image);
+//         reader.onload = () => {
+//             let base64 = reader.result;
+//             console.log(base64);
+//             let base64Input = document.createElement("input");
+//             base64Input.setAttribute("type", "hidden");
+//             base64Input.setAttribute("name", "base64Image");
+//             base64Input.setAttribute("id", "base64Image");
+//             base64Input.setAttribute("value", base64);
+//             document.getElementById("add_plant").appendChild(base64Input);
+
+// 			openUsernameIDB().then((db) => {
+// 				getUsername(db).then((res) => {
+// 					const sessionAuthor = res.value;
+
+// 					let username = document.createElement("input");
+// 					username.setAttribute("type", "hidden");
+// 					username.setAttribute("name", "user_nickname");
+// 					username.setAttribute("id", "user_nickname");
+// 					username.setAttribute("value", sessionAuthor);
+// 					document.getElementById("add_plant").appendChild(username);
+// 				});
+
+// 				// Check if the user is online
+// 				if (navigator.onLine) {
+// 					// syncPlants()
+					
+// 					document.getElementById("add_plant").submit()
+// 				} else {
+// 					// If offline, store the data in IndexedDB
+// 					storePlantInIDB();
+					
+// 					navigator.serviceWorker.ready
+// 					.then(function (serviceWorkerRegistration) {
+// 						serviceWorkerRegistration.showNotification("Plant added while offline!",
+// 							{body: "Please refresh the page when back online..."})
+// 					});
+	
+// 					window.location.href = '/';
+// 				}
+// 			});
+
+            
+//         };
+//         reader.onerror = () => {
+//             console.log("Error converting image");
+//         };
+//     }
+// });
+
+import { addPlantToSync, openUsernameIDB, getUsername, getAllAddedPlantsToSync, deleteSyncedPlants, syncPlants } from './idb-utility.js';
 
 document.getElementById("add_plant").addEventListener("submit", function(e) {
     e.preventDefault();
@@ -18,23 +77,35 @@ document.getElementById("add_plant").addEventListener("submit", function(e) {
             base64Input.setAttribute("value", base64);
             document.getElementById("add_plant").appendChild(base64Input);
 
-            // Check if the user is online
-            if (navigator.onLine) {
-				// syncPlants()
-                
-				document.getElementById("add_plant").submit()
-            } else {
-                // If offline, store the data in IndexedDB
-                storePlantInIDB();
-				
-				navigator.serviceWorker.ready
-				.then(function (serviceWorkerRegistration) {
-					serviceWorkerRegistration.showNotification("Plant added while offline!",
-						{body: "Please refresh the page when back online..."})
-				});
+            openUsernameIDB().then((db) => {
+                getUsername(db).then((res) => {
+                    const sessionAuthor = res.value;
 
-				window.location.href = '/';
-            }
+                    let username = document.createElement("input");
+                    username.setAttribute("type", "hidden");
+                    username.setAttribute("name", "user_nickname");
+                    username.setAttribute("id", "user_nickname");
+                    username.setAttribute("value", sessionAuthor);
+                    document.getElementById("add_plant").appendChild(username);
+
+                    // Check if the user is online
+                    if (navigator.onLine) {
+                        document.getElementById("add_plant").submit();
+                    } else {
+                        // If offline, store the data in IndexedDB
+                        storePlantInIDB(sessionAuthor, base64);
+
+                        navigator.serviceWorker.ready
+                            .then(function (serviceWorkerRegistration) {
+                                serviceWorkerRegistration.showNotification("Plant added while offline!", {
+                                    body: "Please refresh the page when back online..."
+                                });
+                            });
+
+                        window.location.href = '/';
+                    }
+                });
+            });
         };
         reader.onerror = () => {
             console.log("Error converting image");
@@ -43,32 +114,36 @@ document.getElementById("add_plant").addEventListener("submit", function(e) {
 });
 
 function storePlantInIDB() {
-    let plantData = {
-        date: document.getElementById('date_time_seen').value,
-		longitude:  document.getElementById('longitude').value,
-		latitude:  document.getElementById('latitude').value,
-        description: document.getElementById('description').value,
-        size: {
-            height: document.getElementById('plant_height').value,
-            spread: document.getElementById('plant_spread').value
-        },
-        characteristics: {
-            flowers: document.querySelector('[name="flowers"]').checked,
-            leaves: document.querySelector('[name="leaves"]').checked,
-            fruits: document.querySelector('[name="fruits"]').checked,
-            thorns: document.querySelector('[name="thorns"]').checked,
-            seeds: document.querySelector('[name="seeds"]').checked
-        },
-        sunExposure: document.getElementById('sun_exposure').value,
-		identification: {
-			name: document.getElementById('identification_name').value,
-			status: "In Progress",
-		},
-		photo: document.getElementById('base64Image').value,
-		user: document.getElementById('user_nickname').value,
-		chatMessages: [] 
-    };
+            
+            
+            // Construct plantData inside the promise
+            let plantData = {
+                date: document.getElementById('date_time_seen').value,
+                longitude: document.getElementById('longitude').value,
+                latitude: document.getElementById('latitude').value,
+                description: document.getElementById('description').value,
+                size: {
+                    height: document.getElementById('plant_height').value,
+                    spread: document.getElementById('plant_spread').value
+                },
+                characteristics: {
+                    flowers: document.querySelector('[name="flowers"]').checked,
+                    leaves: document.querySelector('[name="leaves"]').checked,
+                    fruits: document.querySelector('[name="fruits"]').checked,
+                    thorns: document.querySelector('[name="thorns"]').checked,
+                    seeds: document.querySelector('[name="seeds"]').checked
+                },
+                sunExposure: document.getElementById('sun_exposure').value,
+                identification: {
+                    name: document.getElementById('identification_name').value,
+                    status: "In Progress",
+                },
+                photo: document.getElementById('base64Image').value,
+                user: document.getElementById('user_nickname').value,
+                chatMessages: [] 
+            };
 
-    // Use the IndexedDB utility to store the plant
-    addPlantToSync(plantData);
+            // Use the IndexedDB utility to store the plant
+            addPlantToSync(plantData);
+
 }
